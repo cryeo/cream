9273c354fe8abde715af778f1284d090fa921f17
Chaerim YEO <yeochaerim@gmail.com>
2016-10-28 14:51:17 +0900

    Add common module

---
+++ cream/common/common.hpp
@@ -0,0 +1,12 @@
+/**************************************************************
+ * Copyright (c) 2016 Chaerim YEO
+ *
+ * Distributed under the MIT License.
+ **************************************************************/
+#ifndef CREAM_COMMON_COMMON_HPP
+#define CREAM_COMMON_COMMON_HPP
+
+#define CREAM_PUBLIC_NAMESPACE(NAME)  namespace NAME {}; namespace
+#define CREAM_PRIVATE_NAMESPACE(NAME) namespace NAME
+
+#endif // CREAM_COMMON_COMMON_HPP
